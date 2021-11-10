672d68a177ae8d7d0dee511fc85c256839fc8fe6
lazo <lazo28.09.77@gmail.com>
2021-11-10 10:09:48 +0200

    fix classwork-1.js

--- independentWork-1/classwork-1.js
+++ independentWork-1/classwork-1.js
@@ -1,8 +1,10 @@
 export const getHolidayPrizes = (name, birthday, sum) => {
-let today = new Date();
-let birthDate = new Date(Date.parse(birthday));
-if (birthDate.getMonth() == today.getMonth() && birthDate.getDate() == today.getDate()) {
-  return `У ${name} сегодня ДР премия = ${sum * 0.1 +
-     (birthDate.getMonth() * birthDate.getDate()) }`;
-}
+  let today = new Date();
+  let birthDate = new Date(Date.parse(birthday));
+  let bonusRaw = sum * 0.1 + (birthDate.getMonth() * birthDate.getDate());
+  let bonus = Math.round(bonusRaw / 10) * 10;
+
+  if (birthDate.getMonth() == today.getMonth() && birthDate.getDate() == today.getDate()) {
+    return `У ${name} сегодня праздник, его премия составляет ${bonus}`;
+  }
 };
